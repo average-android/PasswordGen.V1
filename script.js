e6734b321bfde159d28fdb77800eff55c0251419
Mitchel Busnel <staysaucy555@gmail.com>
2022-01-09 22:18:32 -0500

    Edited CSS file path, finished touches on javascript: still one quirk to work out

--- script.js
+++ script.js
@@ -1,39 +1,59 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var passwordstart = '';
+/*var passwordstart = '';*/
 
-var passwordlength = prompt("How long would you like your password to be?")
 
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+/*var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var lowercase = "abcdefghijklmnopqrstuvwxyz"
 var numbers = "0123456789"
-var specials = "!@#$%^&*()-_=+?/<>,."
+var specials = "!@#$%^&*()-_=+?/<>,."*/
 
-function generatepassword() {
+function generatePassword() {
 
-confirm("Press ok if you want Uppercase characters in your password")
-
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "0123456789";
+  var specials = "!@#$%^&*()-_=+?/<>,.";
+  var passwordstart = '';
+  var password = "";
+ 
   
-for (let i = 0; i < passwordlength; i++ ) {
+  var uppercaseConfirm = confirm("Press OK if you want Uppercase characters in your password.");
+
+  if (uppercaseConfirm) {passwordstart += uppercase}
+
+  var lowercaseConfirm = confirm("Press OK if you want Lowercase characters.");
+
+  if (lowercaseConfirm) {passwordstart += lowercase}
+
+  var numbersConfirm = confirm("Press OK if you want numbers in your password.");
+
+  if (numbersConfirm) {passwordstart += numbers}
+
+  var specialsConfirm = confirm("Press OK if you'd like special characters in your password.");
+
+  if (specialsConfirm) {passwordstart += specials}
+
+  if (!uppercaseConfirm && !lowercaseConfirm && !numbersConfirm && !symbolsConfirm) {alert("Please choose at least one character type.");return}
+
+  var passwordlength = prompt("How long would you like your password to be?");
+
+  if (passwordlength < 8 || passwordlength > 128) {alert("Please choose a number between 8 and 128.");return}
+  
+  for (let i = 0; i < passwordlength; i++ ) {
   password +=
     passwordstart[Math.floor(Math.random() * passwordstart.length)];
-}
-return password;
+  }
+  return password;
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-
- 
- 
   
-
- 
-
   passwordText.value = password;
 }
 
